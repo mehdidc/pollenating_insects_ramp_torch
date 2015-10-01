62180369c2d3f740476ef7b3ed73b9628189fa68
mehdi <mehdi@cherti.name>
2015-10-01 18:57:02 +0200

    model

--- model.lua
+++ model.lua
@@ -1,5 +1,5 @@
 function model(X_train, y_train, X_test)
-    proba = torch.rand(X_test:size(1), 2)
+    proba = torch.rand(X_test:size(1), 18)
     targets = argmax_2D(proba) - 1
     return targets, proba
 end
